8daf0873c45f60182f09e69b07600dc61727c3dd
raulvg01 <9005691@alumnos.ufv.es>
2023-10-10 12:09:08 +0200

    Saludo personalizado

--- pruebas/src/main/java/org/example/Main.java
+++ pruebas/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Cuál es tu nombre?");
+        String name = scanner.nextLine();
+        System.out.println("¡Hola " + name + "!");
     }
+
+
 }
