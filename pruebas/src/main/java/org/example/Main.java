89420cff5d5b460a8450b5c60ad9f649142a35d9
raulvg01 <9005691@alumnos.ufv.es>
2023-10-10 12:17:13 +0200

    Saludo diario

--- pruebas/src/main/java/org/example/Main.java
+++ pruebas/src/main/java/org/example/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println("¿Cuál es tu edad?");
         int age = scanner.nextInt();
         System.out.println("Tu edad es " + age + " años");
+
+        System.out.println("Hoy es: " + LocalDate.now());
     }
 
 
